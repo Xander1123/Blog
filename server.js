8e248ee4cca97c585ead1c8f93c016655bdb0d2e
Nurlan Rajabov <nrajabov@evakuasiya.az>
2025-07-03 10:36:50 +0400

    sql elave edildi server.js ya

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const sqlite3 = require("sqlite3").verbose(); // SQLite3 modulu idxal edin
+
+const db = new sqlite3.Database('./database.db'); // Verilənlər bazası təyin edin
 
 const PORT = process.env.PORT || 3000;
 
 // Static fayllar
 app.use(express.static(path.join(__dirname, "public")));
 
+// Rotlar
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html")); // Əgər views kök kataloqda olarsa
+  res.sendFile(path.join(__dirname, "public", "views", "index.html"));
 });
 app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname,  "views", "admin.html")); // Əgər admin.html public/views-dədirsə
+  res.sendFile(path.join(__dirname, "public", "views", "admin.html"));
 });
 app.get("/add", (req, res) => {
-  res.sendFile(path.join(__dirname,  "views", "add.html")); // Əgər admin.html public/views-dədirsə
+  res.sendFile(path.join(__dirname, "public", "views", "add.html"));
 });
 app.get("/post-details/:title", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "post-details.html")); // Əgər admin.html public/views-dədirsə
+  res.sendFile(path.join(__dirname, "public", "views", "post-details.html"));
 });
 
+// Posts API
+app.get('/posts', (req, res) => {
+  db.all('SELECT * FROM posts ORDER BY created_at DESC', [], (err, rows) => {
+      if (err) return res.status(500).json({ error: 'Database error' });
+      res.json(rows);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
