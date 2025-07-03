3e5e10274b4cd8b9f104ab574b20ee342dd7840a
Nurlan Rajabov <nrajabov@evakuasiya.az>
2025-07-03 09:49:15 +0400

    aa

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+// Static fayllar
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ana səhifə
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
