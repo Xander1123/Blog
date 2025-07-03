7514955703283891f7ff66e91996ba29d07dcc3b
Nurlan Rajabov <nrajabov@evakuasiya.az>
2025-07-03 10:31:42 +0400

    Kecidler elave olundu

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html")); // Əgər views kök kataloqda olarsa
 });
 app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "views", "admin.html")); // Əgər admin.html public/views-dədirsə
+  res.sendFile(path.join(__dirname,  "views", "admin.html")); // Əgər admin.html public/views-dədirsə
 });
 app.get("/add", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "views", "add.html")); // Əgər admin.html public/views-dədirsə
+  res.sendFile(path.join(__dirname,  "views", "add.html")); // Əgər admin.html public/views-dədirsə
 });
 app.get("/post-details/:title", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "views", "post-details.html")); // Əgər admin.html public/views-dədirsə
+  res.sendFile(path.join(__dirname, "views", "post-details.html")); // Əgər admin.html public/views-dədirsə
 });
 
 
