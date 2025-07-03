2ccc54f97decf1098ff0801576cbe3fd08652602
Nurlan Rajabov <nrajabov@evakuasiya.az>
2025-07-03 10:28:23 +0400

    kecidler qoyuldu

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html")); // Əgər views kök kataloqda olarsa
 });
+app.get("/admin", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "views", "admin.html")); // Əgər admin.html public/views-dədirsə
+});
+app.get("/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "views", "add.html")); // Əgər admin.html public/views-dədirsə
+});
+app.get("/post-details/:title", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "views", "post-details.html")); // Əgər admin.html public/views-dədirsə
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
